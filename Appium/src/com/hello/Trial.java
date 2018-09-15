e58f17f62008911c9e84748a118d8ae3b6ba11d5
nehabishnoi <nehabishnoi_23@yahoo.co.in>
2018-09-16 02:24:18 +0530

    Deleted Artificat Project and added Appium Project

--- Appium/src/com/hello/Trial.java
+++ Appium/src/com/hello/Trial.java
@@ -1,36 +1,31 @@
-package Group.Artifact;
+package com.hello;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.annotations.Test;
-
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 
-public class AppTest {
+public class Trial {
 
-	@Test
-	public static void Browser() throws MalformedURLException, InterruptedException {
+	public static void main(String[] args) throws InterruptedException, MalformedURLException {
 
-		System.setProperty("webdriver.chrome.driver",
-				"E:\\software\\Android Softwares\\Drivers\\for v2.40 chromedriver_win32\\chromedriver_win32\\chromedriver.exe");
 		DesiredCapabilities cap = new DesiredCapabilities();
-
 		cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "21ac4f0128017ece");
-		cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "8.0.0");
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Nexus 5X API 28");
+		cap.setCapability(MobileCapabilityType.UDID, "emulator-5554");
+		// cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "7.0");
 		cap.setCapability(MobileCapabilityType.APPIUM_VERSION, "1.9.0");
 		cap.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
-		AppiumDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("https://127.0.0.1:4723/wd/hub"),cap);
+		AppiumDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("https://0.0.0.0:4723/wd/hub"),
+				cap);
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.get("https://uk.virginmoney.com");
-		Thread.sleep(5000);
-
+		Thread.sleep(3000);
+		driver.quit();
 	}
 
 }
