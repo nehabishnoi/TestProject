b6014c459e72807a0b02111273fa1e08c77a5a59
nehabishnoi <nehabishnoi_23@yahoo.co.in>
2018-09-16 03:55:21 +0530

    Deleted Hello File

--- Appium/src/com/hello/Trial.java
+++ Appium/src/com/hello/Trial.java
@@ -3,10 +3,13 @@ package com.hello;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidKeyCode;
 import io.appium.java_client.remote.MobileCapabilityType;
 
 public class Trial {
@@ -15,16 +18,18 @@ public class Trial {
 
 		DesiredCapabilities cap = new DesiredCapabilities();
 		cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Nexus 5X API 28");
-		cap.setCapability(MobileCapabilityType.UDID, "emulator-5554");
-		// cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "7.0");
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Galaxy S8");
+		cap.setCapability(MobileCapabilityType.UDID, "ce061826654a4d1f03");
+		// cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "8.0");
 		cap.setCapability(MobileCapabilityType.APPIUM_VERSION, "1.9.0");
 		cap.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
-		AppiumDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("https://0.0.0.0:4723/wd/hub"),
+		AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("https://0.0.0.0:4723/wd/hub"),
 				cap);
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		driver.get("https://uk.virginmoney.com");
-		Thread.sleep(3000);
+		driver.get("https://www.google.com");
+		driver.findElement(By.name("q")).sendKeys("Neha Rani");
+		driver.pressKeyCode(66);
+		Thread.sleep(6000);
 		driver.quit();
 	}
 
